f9d9281e12a6c4ccf87cae3b28dc7448ef1c1c5b
chenxiaozheng <chenxiaozheng3@163.com>
2014-08-19 13:25:41 +0800

    BStree.c
    完善程序，现在程序已经可以进行正常的创建二叉树的节点，并可以插入到二叉树里，同时采用中序遍历打印出节点

--- 001.c
+++ 001.c
@@ -1,8 +1,99 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "BStree.h"
+
+TREE_INIT_HEAD(head, 40);
+
 
 int main(int argc, char *argv[])
 {
+    printf("head.num = %d\n", head.num);
+    
+    insert_tree_node(create_tree_node(10));
+    insert_tree_node(create_tree_node(18));
+    insert_tree_node(create_tree_node(12));
+    insert_tree_node(create_tree_node(1));
+    insert_tree_node(create_tree_node(9));
+    insert_tree_node(create_tree_node(3));
+    insert_tree_node(create_tree_node(50));
+    insert_tree_node(create_tree_node(70));
+    insert_tree_node(create_tree_node(100));
+    insert_tree_node(create_tree_node(10000));
+
+    print_tree_num(&head);
+
+
+    return 0;
+}
+
+
+struct tree_node *create_tree_node(int num)
+{
+    struct tree_node *dest = (struct tree_node *)malloc(sizeof(struct tree_node));
+    if(dest == NULL)
+    {
+    	printf("[ERROR]: create tree node failed\n");
+        return NULL;
+    }
+
+    else
+    {
+        dest->num = num;
+        dest->left = NULL;
+        dest->right = NULL;
+    }
+
+    return dest;
+}
+
+void insert_tree_node(struct tree_node * dest)
+{
+    if(dest == NULL)
+    {
+        printf("[ERROR]: dest node is NULL\n");
+        return;
+    }
+
+    struct tree_node * ptree_node;
+    for(ptree_node = &head; ptree_node != NULL; )
+    {
+        if(ptree_node->num > dest->num)
+        {
+            if(ptree_node->left == NULL)
+            {
+                ptree_node->left = dest;
+                break;
+            } 
+            else
+               ptree_node = ptree_node->left;
+        }
+        else if(ptree_node->num < dest->num)
+        {
+            if(ptree_node->right == NULL)
+            {
+            	ptree_node->right = dest;
+            	break;
+            }                
+            else
+               ptree_node = ptree_node->right;
+        }
+        else
+            printf("[WARNING]: the insert num had exist.\n");
+    }
     
 }
+
+void print_tree_num(struct tree_node *dest)
+{
+    struct tree_node *p_tree = dest;
+    if(p_tree)
+    {
+        print_tree_num(p_tree->left);
+        printf("p_tree.num = %d\n", p_tree->num);
+        print_tree_num(p_tree->right);
+    }
+
+    return ;
+}
+
